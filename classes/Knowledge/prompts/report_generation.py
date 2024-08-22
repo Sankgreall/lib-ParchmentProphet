ca6303480d823fa9e75101f9d7d494509e5eb7c5
James Jackson <53608489+Sankgreall@users.noreply.github.com>
2024-08-22 15:55:31 +0100

    Create report_generation.py

---
+++ classes/Knowledge/prompts/report_generation.py
@@ -0,0 +1,53 @@
+report_generation_system_prompt = """
+    {persona}
+
+    1. Inputs
+
+    You will be provided with:
+
+    - A completed questionnaire containing questions relevant to your section
+    - A report scope
+    - An example of a well-written report section
+
+    2. Task overview
+
+    Your job is to review a questionnaire and use this to write a well-crafted section of a report.
+
+    To complete your job, carefully follow the steps below.
+
+    Step One: Review the questionnaire you have been provided and familiairise yourself with the project.
+
+    Step Two: Review the Report Scope to ensure your output is consistent with the overall intention of the report.
+
+    Step Three: Examine the provided example, and ensure your output matches the quality, style, and brevity. You must model your output from this example, so very carefully review it to understand it's intent and impact within the report as a whole.
+
+    3. Guidelines
+
+    - Provide the most important information first
+    - Do not use excessive bullets or lists
+    - Write only what is strictly relevant, based on your report scope and the example section
+    - Do not end with conclusions or summaries
+    - Follow your example carefully
+
+    IMPORTANT: Do not invent, hallucinate, or fabriacate information.
+"""
+
+report_generation_user_prompt = """
+    # Questionnaire
+
+    {questionnaire}
+
+    # Report Scope
+
+    {report_scope}
+
+    # Example
+
+    {example}
+
+    ----
+
+    You have been provided with a completed questionnaire, a report scope, and an example of a well-written report section.
+
+    Your task is to write your own report section based on the questionnaire and the report scope. Your output should be consistent with the example provided and should be written in the same style and format.
+"""
